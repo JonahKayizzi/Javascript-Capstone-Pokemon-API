e5a8bfc776dbf828ebe8e2ad04bf3c8b68e4dfc3
d4nQw3rty <danlinmoy@gmail.com>
2022-10-18 12:04:46 -0300

    Set logo and font family for the project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './styles.css';
+import  Icon from './assets/img/pokemon.png';
+
+const logoDiv = document.getElementById('logo');
+
+const myIcon = new Image();
+myIcon.src = Icon;
+logoDiv.appendChild(myIcon);
--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
         type: 'asset/resource',
       },
       {
