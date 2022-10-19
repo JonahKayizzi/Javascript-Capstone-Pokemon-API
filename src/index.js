998f80bbe9f50ed3b53e4009f66dc2dfc98440a6
d4nQw3rty <danlinmoy@gmail.com>
2022-10-19 16:41:30 -0300

    add event listener to display and close popup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import './styles.css';
 import Icon from './assets/img/pokemon.png';
 import displayPokemons from './modules/displayPokemons.js';
+import PopUp from './modules/popUp.js';
 
 const logoDiv = document.getElementById('logo');
+const pokemonsContainer  = document.querySelector('.pokemons-container');
+const modal = document.getElementById('myModal');
+const modalContent = document.querySelector('.modal-content');
 
 const myIcon = new Image();
 myIcon.src = Icon;
@@ -12,3 +16,18 @@ logoDiv.appendChild(myIcon);
 window.onload = () => {
   displayPokemons();
 };
+
+pokemonsContainer.addEventListener('click', (e) => {
+    if (e.target.classList.contains('comments')) {      
+        modal.style.display = 'block';
+        PopUp(e.target.id);        
+    }
+});
+
+modal.addEventListener('click', (e) => {
+    if (e.target.classList.contains('close')) {
+        modal.style.display = 'none'; 
+        modalContent.innerHTML = '';
+    }
+});
+
