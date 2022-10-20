2b69b5f1291903e6d48658f4a334b90821e0f799
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2022-10-20 19:31:07 +0300

    Rebase dev to current branch and fix linter errors

--- src/modules/getLikes.js
+++ src/modules/getLikes.js
@@ -1,6 +1,6 @@
 export default async (pokemonID) => {
   const response = await fetch(
-    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/J8kNrW25pXGkaMtl5ivW/likes/'
+    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/J8kNrW25pXGkaMtl5ivW/likes/',
   );
   const result = await response.json();
   const obj = result.find((o) => o.item_id === pokemonID);
